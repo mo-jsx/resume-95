80fa644b93288dbafda22c9baf126d90afa352a1
mojsx <mojsx.dev@gmail.com>
2023-04-14 19:57:00 +0200

    chores: add w95 icons.

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Bin } from "./bin.png";
+export { default as Computer } from "./computer.png";
+export { default as Dir } from "./directory.png";
+export { default as Start } from "./start32.webp";
