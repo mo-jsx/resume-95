56f18698b4484008d7969e8a11ca5896a0823339
mojsx <mojsx.dev@gmail.com>
2023-04-15 05:11:03 +0200

    feat: add open window on click.

--- src/layouts/Desktop/Desktop.tsx
+++ src/layouts/Desktop/Desktop.tsx
@@ -2,6 +2,8 @@ import "./desktop.scss";
 import { Bin, Computer, Dir } from "../../assets/icons";
 import { FileManager, Icon } from "../../components";
 import { IconProps } from "types/";
+import FileManagerStore from "../../store/FileManagerStore";
+import { useEffect, useState } from "react";
 
 interface IconPlaceholder extends IconProps {
 	title: string;
@@ -14,6 +16,14 @@ const initIcons: IconPlaceholder[] = [
 ];
 
 const Desktop = () => {
+	const [dirState, setDirState] = useState(false);
+
+	const { updateDir, dirStoreState } = FileManagerStore((state) => ({
+		dirStoreState: state.documentsState,
+		updateDir: state.updateDocumentsState,
+	}));
+
+	useEffect(() => {}, [dirState]);
 	return (
 		<div className="desktop">
 			<div className="space">
@@ -22,10 +32,34 @@ const Desktop = () => {
 						className="iconi"
 						id={`icon${index + 1}`}
 						title={icon.title}
-						key={index}>
+						key={index}
+						onClick={() => {
+							setDirState(dirStoreState);
+							updateDir(true);
+						}}>
 						<Icon label={icon.label} img={icon.img} />
 					</div>
 				))}
+
+				{dirState && (
+					<FileManager
+						name={"Documents"}
+						icon={Dir}
+						updateState={() => {
+							setDirState(dirStoreState);
+							updateDir(false);
+						}}>
+						{initIcons.map((icon, index) => (
+							<div
+								className="iconi"
+								id={`icon${index + 1}`}
+								title={icon.title}
+								key={index}>
+								<Icon label={icon.label} img={icon.img} />
+							</div>
+						))}
+					</FileManager>
+				)}
 			</div>
 		</div>
 	);
