a277639657d16e45a05c9d883d79a400baa6b468
mojsx <mojsx.dev@gmail.com>
2023-04-15 14:52:12 +0200

    feat: add draggable.

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,14 +1,17 @@
+import Draggable from "react-draggable";
 import { IconProps } from "types/";
 import "./icon.scss";
 
 const Icon = (props: IconProps) => {
-	const { label, img } = props;
+	const { label, img, variant } = props;
 
 	return (
-		<div className="icon">
-			<img src={img} alt={`${img} logo`} />
-			<p>{label}</p>
-		</div>
+		<Draggable>
+			<div className="icon box">
+				<img src={img} alt={`${img} logo`} unselectable="on" />
+				<p className={`${variant}`}>{label}</p>
+			</div>
+		</Draggable>
 	);
 };
 
