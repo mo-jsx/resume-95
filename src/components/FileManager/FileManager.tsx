ae801a328f5acbd355b2e12cf9b1a3d8056e3445
mojsx <mojsx.dev@gmail.com>
2023-04-15 14:50:43 +0200

    feat: add Draggable to window header.

--- src/components/FileManager/FileManager.tsx
+++ src/components/FileManager/FileManager.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { FileManagerProps, ButtonProps } from "types/";
-import "./fileManager.scss";
-import { Close, Mini, Max } from "../../assets/icons";
 import Button from "../Button";
+import { Close } from "../../assets/icons";
+import "./fileManager.scss";
+import Draggable from "react-draggable";
 
 interface ControlProps extends ButtonProps {
 	title: string;
@@ -18,43 +18,47 @@ const FileManager = (props: FileManagerProps) => {
 	const { name, icon, children, updateState } = props;
 
 	return (
-		<div className="window">
-			<div className="firstRow">
-				<div className="left">
-					<img src={icon} alt={`${name} icon`} />
-					<h4>{name}</h4>
+		<Draggable handle="#handle">
+			<div className="window">
+				<div className="firstRow" id="handle">
+					<div className="left">
+						<img src={icon} alt={`${name} icon`} />
+						<h4>{name}</h4>
+					</div>
+
+					<div className="right">
+						{controls.map((control, index) => (
+							<div
+								className="control"
+								onClick={() => updateState()}>
+								<Button
+									label={control.label}
+									img={control.img}
+									variant={"primary"}
+									key={index}
+									title={control.title}
+								/>
+							</div>
+						))}
+					</div>
 				</div>
 
-				<div className="right">
-					{controls.map((control, index) => (
-						<div className="control" onClick={() => updateState()}>
-							<Button
-								label={control.label}
-								img={control.img}
-								variant={"primary"}
-								key={index}
-								title={control.title}
-							/>
-						</div>
-					))}
+				<div className="secondRow">
+					<ul>
+						{options.map((option, index) => (
+							<li key={index}>{option}</li>
+						))}
+					</ul>
+				</div>
+
+				<div className="playground">{children}</div>
+				<div className="bar">
+					<p>
+						{children ? children.length : 0} object{"("}s{")"} found
+					</p>
 				</div>
 			</div>
-
-			<div className="secondRow">
-				<ul>
-					{options.map((option, index) => (
-						<li key={index}>{option}</li>
-					))}
-				</ul>
-			</div>
-
-			<div className="playground">{children}</div>
-			<div className="bar">
-				<p>
-					{children ? children.length : 0} object{"("}s{")"} found
-				</p>
-			</div>
-		</div>
+		</Draggable>
 	);
 };
 
