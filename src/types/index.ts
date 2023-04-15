ef34c470e742daf0937f3ceabefcf11248dfb26a
mojsx <mojsx.dev@gmail.com>
2023-04-15 02:39:21 +0200

    chores: add single export FileManagerProps.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export type { default as ButtonProps } from "./ButtonProps";
 export type { default as IconProps } from "./IconProps";
+export type { default as FileManagerProps } from "./FileManagerProps";
