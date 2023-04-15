6bd65c93a7d73ddacc525c910928744eaae70815
mojsx <mojsx.dev@gmail.com>
2023-04-15 02:32:34 +0200

    chores: add single exports for Close, Max, Mini icons.

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,6 @@ export { default as Bin } from "./bin.png";
 export { default as Computer } from "./computer.png";
 export { default as Dir } from "./directory.png";
 export { default as Start } from "./start32.webp";
+export { default as Close } from "./close.png";
+export { default as Max } from "./max.gif";
+export { default as Mini } from "./mini.gif";
