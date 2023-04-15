18a42fdd577619d2c37594c7145bbacd7820fdd6
mojsx <mojsx.dev@gmail.com>
2023-04-15 02:33:42 +0200

    refactor: add image-only button.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,12 +5,18 @@ const Button = (props: ButtonProps) => {
 	const { label, img, alt, variant = "normal" } = props;
 	return (
 		<>
-			{img ? (
+			{img && label && (
 				<button className={`btn ${variant}`}>
 					<img src={img} alt={alt} />
 					<span className="label">{label}</span>
 				</button>
-			) : (
+			)}
+			{img && !label && (
+				<button className={`btn ${variant}`}>
+					<img src={img} alt={`${img} icon`} />
+				</button>
+			)}
+			{!img && label && (
 				<button className={`btn ${variant}`}>{label}</button>
 			)}
 		</>
