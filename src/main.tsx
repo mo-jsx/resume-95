7ac2f41ef2680ddb1b22f98b8356289f310b889e
mojsx <mojsx.dev@gmail.com>
2023-04-18 18:48:46 +0200

    chores: remove Strict Mode.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,5 @@ import App from "./App";
 import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<App />
 );
