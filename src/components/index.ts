517759b8e62ba28f4fc80f34fd5a90e51de0eb2b
mojsx <mojsx.dev@gmail.com>
2023-04-19 23:52:26 +0200

    chores: rename FileManager => Window.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from "./Button";
 export { default as Icon } from "./Icon";
-export { default as FileManager } from "./FileManager";
+export { default as Window } from "./Window";
