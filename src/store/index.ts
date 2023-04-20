778ad6b2f5f00e3d407a32f6605961a6b411e77a
mojsx <mojsx.dev@gmail.com>
2023-04-20 19:43:51 +0200

    store: add maxmize, minimize actions.

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,19 +6,36 @@ interface ProcessManagerProps {
 
 	createProcess: (newWindow: WindowProps) => void;
 	killProcess: (id: string) => void;
+	maximize: (id: string, newState: boolean) => void;
+	minimize: (id: string, newState: boolean) => void;
 }
 
-const ProcessManager = create<ProcessManagerProps>()((set) => ({
+const ProcessManager = create<ProcessManagerProps>()((set, get) => ({
 	openWindows: [],
 
-	createProcess: (newWindow) =>
-		set((state) => ({ openWindows: state.openWindows.concat(newWindow) })),
+	createProcess: (newWindow) => {
+		set((state) => ({ openWindows: state.openWindows.concat(newWindow) }));
+	},
 	killProcess: (id) =>
 		set((state) => ({
 			openWindows: state.openWindows.filter(
 				(openWindow) => openWindow.id !== id
 			),
 		})),
+	maximize: (id, newState) => {
+		const openWindows = get().openWindows;
+		const updatedWindows = openWindows.map((window) =>
+			window.id === id ? { ...window, isMaximized: newState } : window
+		);
+		set({ openWindows: updatedWindows });
+	},
+	minimize: (id, newState) => {
+		const openWindows = get().openWindows;
+		const updatedWindows = openWindows.map((window) =>
+			window.id === id ? { ...window, isMinimized: newState } : window
+		);
+		set({ openWindows: updatedWindows });
+	},
 }));
 
 export default ProcessManager;
