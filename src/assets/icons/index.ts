0cc3333ca9e4925b86586a28d4dd59a87b92f36f
mojsx <mojsx.dev@gmail.com>
2023-04-20 19:34:23 +0200

    chores: add window control icons.

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,5 @@ export { default as Computer } from "./computer.png";
 export { default as Dir } from "./directory.png";
 export { default as Start } from "./start32.webp";
 export { default as Close } from "./close.png";
-export { default as Max } from "./max.gif";
-export { default as Mini } from "./mini.gif";
+export { default as Maximize } from "./maximize.png";
+export { default as Mini } from "./mini.png";
