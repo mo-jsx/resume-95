041e6d00826c3daa0353bb56e78b827b185434c6
mojsx <mojsx.dev@gmail.com>
2023-04-21 16:24:42 +0200

    component: add Resume component.

---
+++ src/components/Resume/index.tsx
@@ -0,0 +1,41 @@
+import { Printer } from "../../assets/icons";
+import MyResume from "../../assets/data/Resume-1.png";
+
+const Resume = () => {
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "flex-start",
+				width: "100%",
+				overflowY: "scroll",
+				textAlign: "center",
+			}}>
+			<a
+				href="../../assets/data/Resume.pdf"
+				download
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "center",
+					alignContent: "center",
+					width: "auto",
+				}}>
+				<h3>Print Resume</h3>
+				<img
+					src={Printer}
+					alt="Printer icon"
+					style={{
+						margin: "0px 10px",
+						width: "1.5rem",
+						height: "1.5rem",
+					}}
+				/>
+			</a>
+			<img src={MyResume} alt="Resume" />
+		</div>
+	);
+};
+
+export default Resume;
