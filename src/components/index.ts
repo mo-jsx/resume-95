ef97f94b7e37f1ef6a8f1965d7bca173b5c3abd9
mojsx <mojsx.dev@gmail.com>
2023-04-22 01:42:16 +0200

    chores: add single exports for Contact, Projects.

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as Button } from "./Button";
 export { default as Icon } from "./Icon";
 export { default as Window } from "./Window";
 export { default as Resume } from "./Resume";
+export { default as Contact } from "./Contact";
+export { default as Projects } from "./Projects";
