7986e030ae1b1047e4af8c93050bec8faf519818
mojsx <mojsx.dev@gmail.com>
2023-04-22 01:43:10 +0200

    layout: add Project Icon.

--- src/layouts/Desktop/index.tsx
+++ src/layouts/Desktop/index.tsx
@@ -2,7 +2,7 @@
 import ProcessManager from "../../store";
 
 // LAYOUTS & COMPONENTS
-import { Icon, Window, Resume } from "../../components";
+import { Icon, Window, Resume, Contact, Projects } from "../../components";
 
 // ASSETS
 import { Bin, Computer, Dir, File } from "../../assets/icons";
@@ -25,10 +25,22 @@ const Desktop = () => {
 				</div>
 
 				<div id="icon3">
-					<Icon label={"Documents"} img={Dir} />
+					<Icon
+						label={"Contact Links"}
+						img={Dir}
+						children={<Contact />}
+					/>
 				</div>
 
 				<div id="icon4">
+					<Icon
+						label={"Projects"}
+						img={Dir}
+						children={<Projects />}
+					/>
+				</div>
+
+				<div id="icon5">
 					<Icon
 						label={"My resume.pdf"}
 						img={File}
