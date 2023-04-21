b1ce3d71afb5f08edf2e3f2caafe421b27749e45
mojsx <mojsx.dev@gmail.com>
2023-04-21 16:20:45 +0200

    chores: add single export for Resume.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Icon } from "./Icon";
 export { default as Window } from "./Window";
+export { default as Resume } from "./Resume";
