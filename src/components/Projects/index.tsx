dd6ba236bbfee39e09f2108c2f29af8d3fc0f5f7
mojsx <mojsx.dev@gmail.com>
2023-04-22 01:45:25 +0200

    component: add Projects data component.

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,31 @@
+import { Write, Larsson, Toothist } from "../../assets/icons";
+import Icon from "../Icon";
+
+const Projects = () => {
+	return (
+		<div style={{ display: "flex", flexDirection: "row" }}>
+			<a
+				href="https://github.com/mo-jsx/rezumee"
+				target="_blank"
+				title="A web app based resume builder">
+				<Icon label={"Rezumee"} img={Write} variant={"black"} />
+			</a>
+
+			<a
+				href="https://github.com/mo-jsx/larsson"
+				target="_blank"
+				title="A react component library">
+				<Icon label={"Larsson"} img={Larsson} variant={"black"} />
+			</a>
+
+			<a
+				href="https://github.com/mo-jsx/toothist"
+				target="_blank"
+				title="A web app based MCQ platform">
+				<Icon label={"Larsson"} img={Toothist} variant={"black"} />
+			</a>
+		</div>
+	);
+};
+
+export default Projects;
