36e9a89f32435607b0bc6ba3cef594bb15dd97d0
mojsx <mojsx.dev@gmail.com>
2023-04-22 17:25:15 +0200

    chores: add AboutMe single export.

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Window } from "./Window";
 export { default as Resume } from "./Resume";
 export { default as Contact } from "./Contact";
 export { default as Projects } from "./Projects";
+export { default as AboutMe } from "./AboutMe";
