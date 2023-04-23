e5e46e80daea4a8f1bb43581770e56ef9e90e2a3
mojsx <mojsx.dev@gmail.com>
2023-04-23 03:17:20 +0200

    chores: add Menu single export.

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Resume } from "./Resume";
 export { default as Contact } from "./Contact";
 export { default as Projects } from "./Projects";
 export { default as AboutMe } from "./AboutMe";
+export { default as Menu } from "./Menu";
