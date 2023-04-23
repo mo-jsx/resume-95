f4c67c6ed0411994d273b249cb1be404d22db5d2
mojsx <mojsx.dev@gmail.com>
2023-04-23 03:52:09 +0200

    feat: add Vercel Analytics.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { inject } from "@vercel/analytics";
 import "./styles/global.scss";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<App />
